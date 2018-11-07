6c08fdb0f75fe1759ef91178370b8346b04f1732
Alberto Sierra <alberto.sierra@murciaeduca.es>
2018-11-07 14:13:54 +0100

    Solo el propietario puede devolver

--- common/models/Pedidos.js
+++ common/models/Pedidos.js
@@ -1,6 +1,28 @@
 'use strict';
 
 module.exports = function (Pedidos) {
+
+  Pedidos.beforeRemote('create', function(context, pedido, next) {
+    context.args.data.FechaPedido = Date.now();
+    context.args.data.IdCliente = context.req.accessToken.userId;
+    next();
+  });
+  
+    Pedidos.beforeRemote('prototype.devolver', function (ctx, pedido, next) {
+        pedido = ctx.instance;
+        pedido.detallesPedidos({
+            where: {IdProducto: ctx.args.IdProducto}
+        }, function(err, filasDetalles){
+            if (err) next(err);
+            if(filasDetalles.length == 0) {
+                next(new Error('El producto no está en ese pedido'));
+            } else {
+                next();
+            }
+        });
+        
+    });
+
     /**
      * Permite devolver un producto comprado anteriormente
      * @param {number} IdProducto El producto que se quiere devolver
@@ -14,7 +36,7 @@ module.exports = function (Pedidos) {
             productosId: IdProducto,
             Cantidad: Cantidad
         };
-        
+
         pedido.devoluciones.create(data,
                 function (err, devolucion) {
                     callback(err, devolucion);
--- server/boot/create-devoluciones-model.js
+++ server/boot/create-devoluciones-model.js
@@ -1,8 +1,20 @@
 module.exports = function(app) {
-  //data sources
-  var mysqlDs = app.dataSources.mysqlDs;
-      mysqlDs.automigrate('Devoluciones', function(err) {
-      if (err) throw(err);
-      
-    });
+    
+    if(process.env.AUTOMIGRATE == 'true') {
+        //data sources
+        var mysqlDs = app.dataSources.mysqlDs;
+        var db = app.dataSources.db;
+
+          mysqlDs.automigrate('Devoluciones', function(err) {
+            if (err) throw(err);
+
+          });
+          db.automigrate(null, function(err) {
+            if (err) throw(err);
+
+          });
+          mysqlDs.autoupdate('Clientes', function(err){
+            if (err) throw(err);
+          });
+    }
 };
